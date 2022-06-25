e7059635457be461ee8fb20dfc31401a0b1f1497
michaeladavis2308 <64472860+michaeladavis2308@users.noreply.github.com>
2022-06-25 10:58:07 +0000

    Add files via upload

---
+++ Zuri.js
@@ -0,0 +1,7 @@
+let name ="Michaela Davis"
+var height = 167.64
+const country = "Ghana"
+let myFirstCode = `My name is ${name} with a height of ${height}cm and I am from ${country}`
+console.log(myFirstCode);
+alert(myFirstCode);
+document.write(myFirstCode)
